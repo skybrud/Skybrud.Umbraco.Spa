92d8c24173af1981cc61f01626232fbfb68d1109
Anders Bjerner <abjerner@skybrud.dk>
2019-06-06 09:09:58 +0200

    Build stuff

---
+++ release.bat
@@ -0,0 +1,4 @@
+@echo off
+cd src/Skybrud.Umbraco.Spa
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe" /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
+cd ../../
